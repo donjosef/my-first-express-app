d848f4f1bd0b69c14e0be63c8d577bb9ccfb5a4f
donjoe <giumontanaro@gmail.com>
2018-12-20 11:50:17 +0100

    Use home route inside app as middleware

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
 const app = express();
+
+const homeRoute = require('./routes/home');
+
+app.use(homeRoute);
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
