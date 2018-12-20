589edf874a004ffdb402b38eff712a30f72ca975
donjoe <giumontanaro@gmail.com>
2018-12-20 12:06:55 +0100

    Use usersRoute inside app

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const homeRoute = require('./routes/home');
+const usersRoute = require('./routes/users');
 
+app.use(usersRoute);
 app.use(homeRoute);
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
